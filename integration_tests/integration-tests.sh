2df0f4519374a3eec6871f35fb69e9b8a27a10ce
Kunal Hire <kunal.h@affinidi.com>
2022-05-22 20:45:32 +0200

    Update instruction

--- integration_tests/integration-tests.sh
+++ integration_tests/integration-tests.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-echo "Please make sure you have newman and a running json-server(refer setup.sh) before executing integration tests!!"
+echo "Please make sure you have newman and a running server(npm run bootServer) before executing integration tests!!"
 
 newman run books-shop.postman_collection.json
